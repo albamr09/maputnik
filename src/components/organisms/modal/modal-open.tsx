295fb1f2b59816d9b156dfb02e30519860d374e9
albamr09 <bertaalba1@gmail.com>
2025-09-25 20:18:24 +0200

    Fix max height

--- src/components/organisms/modal/modal-open.tsx
+++ src/components/organisms/modal/modal-open.tsx
@@ -126,7 +126,7 @@ const ModalOpen = () => {
             <ScrollArea>
               <div
                 className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"
-                style={{ maxHeight: "22vh" }}
+                style={{ maxHeight: "40vh" }}
               >
                 {publicStyles.map((style) => (
                   <PreviewCard
