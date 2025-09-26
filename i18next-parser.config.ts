c64132e2973b7089509f838f4779bc3d5f222a8f
albamr09 <bertaalba1@gmail.com>
2025-09-26 17:37:05 +0200

    Apply biome

--- i18next-parser.config.ts
+++ i18next-parser.config.ts
@@ -1,20 +1,20 @@
 export default {
-  input: ['src/**/*.{ts,tsx}'],
-  output: 'public/locales/$LOCALE/$NAMESPACE.json',
-  locales: ['en', 'de', 'fr', 'he', 'it', 'ja', 'zh'],
-  defaultLocale: 'en',
-  keySeparator: false,
-  nsSeparator: false,
-  keepRemoved: true,
-  sort: true,
-  skipUntranslated: true,
-  createOldCatalogs: false,
-  useKeysAsDefaultValue: true,
-  debug: false,
-  removeUnusedKeys: true,
-  contextSeparator: '_',
-  interpolation: {
-    prefix: '{{',
-    suffix: '}}'
-  }
+	input: ["src/**/*.{ts,tsx}"],
+	output: "public/locales/$LOCALE/$NAMESPACE.json",
+	locales: ["en", "de", "fr", "he", "it", "ja", "zh"],
+	defaultLocale: "en",
+	keySeparator: false,
+	nsSeparator: false,
+	keepRemoved: true,
+	sort: true,
+	skipUntranslated: true,
+	createOldCatalogs: false,
+	useKeysAsDefaultValue: true,
+	debug: false,
+	removeUnusedKeys: true,
+	contextSeparator: "_",
+	interpolation: {
+		prefix: "{{",
+		suffix: "}}",
+	},
 };
